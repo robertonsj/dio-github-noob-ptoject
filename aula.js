7ae7de324dda619e4aa4ea4a9c5bdaa0c423df1b
Roberto <72029380+robertonsj@users.noreply.github.com>
2022-11-28 02:04:51 +0000

    Variaveis e operadores

--- aula.js
+++ aula.js
@@ -1,5 +1,19 @@
-let num1 = 10;
-let num2 = 5;
-let output = num1 * num2;
+// let num1 = 10;
+// let num2 = 5;
+// let output = num1 * num2;
 
-console.log(`Resultado = ${output}`);
+// console.log(`Resultado = ${output}`);
+
+/**Quantia gasta em uma dada viagem
+ * 1 - preço do combutivel;
+ * 2 - Gasto médio de combustível do carro por KM
+ * 3 - Distância em KM da viagem.
+ */
+
+const fuelPrice = 5.79
+const kmPorLitro = 12
+const distanceKm = 100;
+
+const qtdConsumo = distanceKm / kmPorLitro
+const totalCost = qtdConsumo * fuelPrice
+console.log(`The total cost is $${totalCost.toFixed(2)}`)
