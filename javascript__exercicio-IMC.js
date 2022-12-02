fecc6947fa460193a42fa606805def56bc4183c6
Roberto <72029380+robertonsj@users.noreply.github.com>
2022-12-02 00:01:18 +0000

    Exibir IMC

--- javascript__exercicio-IMC.js
+++ javascript__exercicio-IMC.js
@@ -6,9 +6,9 @@ const imc = peso / altura**2
 let resultado
 
 if (imc < 18.5)  resultado = 'abaixo do peso'
-else if (imc >= 18.5 && imc <= 25) resultado ='com peso normal'
-else if (imc > 25 && imc <= 30) resultado = 'acima do peso'
-else if (imc > 30 && imc <= 40) resutado = 'obeso'
+else if (imc >= 18.5 && imc < 25) resultado ='com peso normal'
+else if (imc >= 25 && imc < 30) resultado = 'acima do peso'
+else if (imc >= 30 && imc <= 40) resultado = 'obeso'
 else resultado = 'com obesidade grave'
 
 console.log(`Voce esta ${resultado}`)
